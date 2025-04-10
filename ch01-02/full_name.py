107d5f4a000e530acfe917e1366e0d3d2b18e9ba
shane-ohanlon_bveng <shane.ohanlon@bazaarvoice.com>
2025-04-10 14:32:31 +0100

    string manipulation exercises

--- ch01-02/full_name.py
+++ ch01-02/full_name.py
@@ -1,4 +1,6 @@
-first_name = "ada"
-last_name = "lovelace"
-full_name = f"{first_name} {last_name}"
+first_name = "ada  "
+last_name = "lovelace "
+full_name = f"{first_name.strip()} {last_name.strip()}"
 print(full_name)
+message = f"Hello, \n\t{full_name.title()}!"
+print(f"{message}")
---
+++ ch01-02/name_message.py
@@ -0,0 +1,8 @@
+full_name = "James McJamesface"
+print(f"Hello, {full_name.title()}!")
+print(f"Hello, {full_name.upper()}!")
+print(f"Hello, {full_name.lower()}!")
+
+famous_person = "  Albert Einstein  "
+quote = '"A person who never made a mistake never tried anything new."'
+print(f'{famous_person.strip()} once said, " \n\t{quote.strip()} ')
